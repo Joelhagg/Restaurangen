bcc1b686077fba7f1f91fabaa24d7392c668b05c
mats363 <81287846+mats363@users.noreply.github.com>
2022-04-11 15:40:01 +0200

    this will add admin functionality

--- src/components/admin/Admin.tsx
+++ src/components/admin/Admin.tsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from 'react';
+import { IReservation } from '../../models/IReservation';
 import { Reservation } from '../../models/Reservation';
 import { BookingService } from '../../services/BookingService';
 import './Admin.scss';
 
 export function Admin () {
     
-    const [bookings, setBookings] = useState<Reservation[]>([]);
+    const [bookings, setBookings] = useState<IReservation[]>([]);
 
+    console.log("bookings" + bookings)
     useEffect(() => {
         let service = new BookingService;
         service.fetchBookings()
         .then(fetchedBookings => setBookings(fetchedBookings));
-        console.log(bookings)
+        console.log()
     }, ([]));
-/*
+
     let bookingsHtml = bookings.map((b) => {
-        return <li key={}>
-        <span>{b.customer}: </span>
+        return <li key={b._id}>
+        <span>{b.customerId} </span>
         <span>{b.date}</span>
+        <button>Remove</button>
       </li>
     }
     )
-*/
+
 
     return(<>
        <h1 className='adminHeader'>Admin works!</h1> 
-       <ul> </ul>
+       <ul> {bookingsHtml}</ul>
     </>)
 }
