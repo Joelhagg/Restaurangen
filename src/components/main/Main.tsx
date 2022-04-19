848d56307b8ffa493a155f88f3e40df053048b6c
joelhagg <joel.pettersson90@gmail.com>
2022-04-19 15:16:38 +0200

    This will update main components

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import './Main.scss';
+import React from "react";
+import "./Main.scss";
 
-export function Main () {
-
-    return (<>
-    <div className='mainWraper'>
-        <div className='mainContainer'>
-            <h1>Main Works!</h1>
+export function Main() {
+  return (
+    <>
+      <div className="mainWraper">
+        <div className="mainContainer">
+          <div className="mainInformationContainer">
+            <h1>Välkommen till Majojo</h1>
+            <h3>
+              Här kan ni avnjuta renskav med dagsfärska primörer för att sedan
+              gå till lägerelden medn en Gin & Tonic
+            </h3>
+          </div>
         </div>
-    </div>
-
-    </>)
+      </div>
+    </>
+  );
 }
