66acbf1a5d97edd1a5e4b6059f30f16835c41544
mats363 <81287846+mats363@users.noreply.github.com>
2022-04-21 13:04:29 +0200

    this will add text to booking page

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
             target="_blank"
             href="https://www.google.com/maps/place/Kolarbyn+Eco-Lodge/@59.8072818,15.7204379,16z/data=!4m16!1m7!3m6!1s0x0:0xd736f156aa2d8b7c!2zNTnCsDQ4JzM3LjIiTiAxNcKwNDAnNTkuOSJF!3b1!8m2!3d59.8103333!4d15.6833056!3m7!1s0x465dcf73e592f96d:0xec6a670ee2744fb9!5m2!4m1!1i2!8m2!3d59.8074118!4d15.7287312"
           >
-            <h3>59°48'37.2"N 15°40'59.9"E</h3>
+            <h4>59°48'37.2"N 15°40'59.9"E</h4>
           </a>{" "}
         </div>
         <Link to="/admin">
