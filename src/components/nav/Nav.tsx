1a0d8bda57d678ded96ea673df7f315066079b5a
Jonas <jonasericsson90@hotmail.com>
2022-04-21 08:25:18 +0200

    this will add burger menu + styling

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,38 +2,56 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Nav.scss";
 import Logo from "../../image/Majojo.jpg";
+import { useState } from "react";
 
-export function Nav() {
+export function Nav()  {
+  const [isActive, setActive] = useState(false);
+
+  const openBurgerMenu = () => {
+    const toggleClass = () => {
+      setActive(!isActive);
+    };
+
+    toggleClass();
+  };
+
+  const closeMenu = () => {
+    setActive(false);
+  };
+
   return (
     <>
-      <header>
+    <header>
         <nav>
           <Link to="/">
             {" "}
-            <img src={Logo} className="logo" />
+            <img src={Logo} className="logo" alt="Logo of Majojo" />
           </Link>
+          </nav>
+          
 
-          <ul>
-            <li>
-              <Link to="/">Hem</Link>
-            </li>
-            <li>
-              <Link to="/menu">Meny</Link>
-            </li>
-            <li>
-              <Link to="/booking">Booking</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <button id="menu-button">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
+      <nav
+        className={`hamburger ${isActive ? "hamburger-active" : ""}`}
+        onClick={openBurgerMenu}
+      >
+        <span className="hamburger__icon"></span>
+      </nav>
+      <nav className={`header-links-container ${isActive ? "change" : ""}`}>
+        <nav className="header-links">
+        <Link to ="/" onClick={closeMenu} >Hem</Link>
+          <Link to ="/menu" onClick={closeMenu} >Meny</Link>
+          <Link to ="/booking" onClick={closeMenu} >Bokning</Link>
+          <Link to ="/contact" onClick={closeMenu} >Kontakta oss</Link>
+          
         </nav>
-      </header>
+      </nav>
+    </header>
     </>
   );
-}
+};
+
+export default Nav;
+
+
+
+ 
